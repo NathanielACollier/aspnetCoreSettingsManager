0488682b87b2d91111f19a15cb2abcbf6e98e06b
Nathaniel Collier <drxs3v3n@gmail.com>
2019-09-04 17:12:00 -0500

    Basic aspnet core site setup with index.html
    Overwrite the index.html with angular build

---
+++ SettingsManagerWebApp/Controllers/GeneralController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SettingsManagerWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneralController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet, Route("test")]
+        public string test()
+        {
+            return "Hello World!";
+        }
+
+
+    }
+}
